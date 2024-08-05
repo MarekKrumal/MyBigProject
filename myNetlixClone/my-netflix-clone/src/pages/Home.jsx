1d9e499c4e076e71fb35ce07fcf2191694332769
Marek Krumal <krumalmarek@gmail.com>
2024-08-05 10:41:53 +0200

    Vytvoreni Row.jsx FetchURL
    stylovani v Row, FetchURL, import FaHeart

--- myNetlixClone/my-netflix-clone/src/pages/Home.jsx
+++ myNetlixClone/my-netflix-clone/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../pages/Main'
+import Row from './Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
         <Main />
+        <Row title="Up Coming" fetchURL={requests.requestUpcoming}/>
+        <Row title="Trending" fetchURL={requests.requestTrending}/>
+        <Row title="Popular" fetchURL={requests.requestPopular}/>
+        <Row title="Top Rated" fetchURL={requests.requestTopRated}/>
+        <Row title="Horror" fetchURL={requests.requestHorror}/>
     </>
   )
 }
