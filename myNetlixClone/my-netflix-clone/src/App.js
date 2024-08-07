aa4ec22c714330f23fe891fe7a744bab42d2c02b
Marek Krumal <krumalmarek@gmail.com>
2024-08-07 08:49:23 +0200

    SignIn, SignUp
    +style

--- myNetlixClone/my-netflix-clone/src/App.js
+++ myNetlixClone/my-netflix-clone/src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/account" element={<Account />}/>
     </Routes>
     </AuthContextProvider>
   </>
